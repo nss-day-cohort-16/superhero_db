4a50b7c8ef2eaebb7c38e1f0c22287ed0f05c44a
Steve Brownlee <chortlehoort@gmail.com>
2017-01-12 14:55:37 -0600

    Added powers

--- superhero.sql
+++ superhero.sql
@@ -10,6 +10,28 @@ CREATE TABLE `Sidekick` (
 	`Name`	TEXT NOT NULL,
 	`Gender`	TEXT NOT NULL,
 	`Profession`	TEXT NOT NULL,
-	`SuperheroId`	INTEGER NOT NULL
+	`SuperheroId`	INTEGER NOT NULL,
 	FOREIGN KEY(`SuperheroId`) REFERENCES `Superhero`(`SuperheroId`)
 );
+
+CREATE TABLE `PowerType` (
+	`PowerTypeId`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`Name`	TEXT NOT NULL
+);
+
+CREATE TABLE `Power` (
+	`PowerId`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`Name`	TEXT NOT NULL,
+	`PowerTypeId`	INTEGER NOT NULL,
+	FOREIGN KEY(`PowerTypeId`) REFERENCES `PowerType`(`PowerTypeId`)
+);
+
+CREATE TABLE `SuperheroPower` (
+	`SuperheroPowerId`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`SuperheroId`	INTEGER NOT NULL,
+	`PowerId`	INTEGER NOT NULL,
+	FOREIGN KEY(`PowerId`) REFERENCES `Power`(`PowerId`),
+	FOREIGN KEY(`SuperheroId`) REFERENCES `Superhero`(`SuperheroId`)
+);
+
+
